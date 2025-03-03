9421bc9947ae68f88eb508c2243c78b2fe26fe29
Sean O'Connor <15949898+seaneoo@users.noreply.github.com>
2025-03-03 13:39:43 -0700

    Add more robust fetching to ApiService

--- app/src/main/java/dev/seano/mcpn/network/api/ApiService.kt
+++ app/src/main/java/dev/seano/mcpn/network/api/ApiService.kt
@@ -5,15 +5,40 @@ import dev.seano.mcpn.network.NetworkError
 import dev.seano.mcpn.network.Result
 import dev.seano.mcpn.network.api.model.VersionManifest
 import dev.seano.mcpn.network.api.model.VersionMetadata
+import dev.seano.mcpn.network.onFailure
+import dev.seano.mcpn.network.onSuccess
 import io.ktor.http.appendPathSegments
+import timber.log.Timber
 
 class ApiService(private val networkClient: NetworkClient) {
 
-    suspend fun getVersionManifest(): Result<VersionManifest, NetworkError> =
-        networkClient.call<VersionManifest>(urlString = "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json")
+    object Endpoints {
 
-    suspend fun getVersionMetadata(id: String, sha1: String) =
-        networkClient.call<VersionMetadata>(urlString = "https://piston-meta.mojang.com/v1/packages") {
-            url { appendPathSegments(sha1, "$id.json") }
+        const val PISTON_META_BASE_URL = "https://piston-meta.mojang.com"
+        const val VERSION_MANIFEST = "mc/game/version_manifest_v2.json"
+        const val VERSION_METADATA = "v1/packages/%s/%s"
+    }
+
+    private suspend inline fun <reified T> fetch(
+        urlString: String,
+        vararg pathSegments: String,
+    ): Result<T, NetworkError> {
+        return networkClient.call<T>(urlString = urlString) {
+            url {
+                appendPathSegments(*pathSegments)
+            }
         }
+            .onSuccess { Timber.d("Successfully fetched ${T::class.simpleName}") }
+            .onFailure { Timber.d("Failed to fetch for ${T::class.simpleName}: ${it.message}") }
+    }
+
+    suspend fun getVersionManifest(): Result<VersionManifest, NetworkError> =
+        fetch<VersionManifest>(Endpoints.PISTON_META_BASE_URL, Endpoints.VERSION_MANIFEST)
+
+    suspend fun getVersionMetadata(
+        id: String,
+        sha1: String,
+    ): Result<VersionMetadata, NetworkError> = fetch<VersionMetadata>(
+        Endpoints.PISTON_META_BASE_URL, Endpoints.VERSION_METADATA.format(sha1, "$id.json")
+    )
 }
