6e83754032d039b91a394429bd6592eba07376aa
Sean O'Connor <15949898+seaneoo@users.noreply.github.com>
2025-03-08 13:39:50 -0700

    Add PatchNotes model with JavaEntry, BedrockEntry, and Image submodels

--- app/src/main/java/dev/seano/mcpn/network/api/model/PatchNotes.kt
+++ app/src/main/java/dev/seano/mcpn/network/api/model/PatchNotes.kt
@@ -6,18 +6,43 @@ import java.time.ZonedDateTime
 
 interface PatchNoteEntry
 
+/**
+ * Patch notes for either Java or Bedrock edition.
+ * @param version N/A
+ * @param entries List of [PatchNotes.JavaEntry] or [PatchNotes.BedrockEntry].
+ */
 @Serializable
 data class PatchNotes<T : PatchNoteEntry>(
     val version: Int,
     val entries: List<T>,
 ) {
 
+    /**
+     * Represents a single image in a patch note.
+     * @param title The image file name.
+     * @param url The url to the image (relative to the API base url). See [getFullUrl].
+     */
     @Serializable
     data class Image(val title: String, val url: String) {
 
+        /**
+         * The full url to the image. See [url].
+         */
         fun getFullUrl() = "https://launchercontent.mojang.com${url}"
     }
 
+    /**
+     * Represents a Java edition patch note.
+     * Identical to [BedrockEntry] with the exception of the [type] param.
+     * @param title
+     * @param version
+     * @param type
+     * @param image
+     * @param contentPath
+     * @param id
+     * @param date
+     * @param shortText
+     */
     @Serializable
     data class JavaEntry(
         val title: String,
@@ -30,6 +55,18 @@ data class PatchNotes<T : PatchNoteEntry>(
         val shortText: String,
     ) : PatchNoteEntry
 
+    /**
+     * Represents a Bedrock edition patch note.
+     * Identical to [JavaEntry] with the exception of the [patchNoteType] param.
+     * @param title
+     * @param version
+     * @param patchNoteType
+     * @param image
+     * @param contentPath
+     * @param id
+     * @param date
+     * @param shortText
+     */
     @Serializable
     data class BedrockEntry(
         val title: String,
